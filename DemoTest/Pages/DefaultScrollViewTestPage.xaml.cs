096df96849aca82be646bd2839c6be85d9970f8f
xtuzy <1570571499@qq.com>
2023-06-14 11:05:02 +0800

    see scrollview how to deal with anim when add and remove child

--- DemoTest/Pages/DefaultScrollViewTestPage.xaml.cs
+++ DemoTest/Pages/DefaultScrollViewTestPage.xaml.cs
@@ -6,21 +6,73 @@ public partial class DefaultScrollViewTestPage : ContentPage
     {
         InitializeComponent();
         var index = 0;
-        while (index < 100)
+        emptyView = new Grid() { HeightRequest = 500 };
+        rootLayout.Children.Add(emptyView);
+        while (index < 15)
         {
             index++;
-            var botCell = new Image();
-            botCell.Source = "dotnet_bot.png";
-            var baiduCell = new Image();
-            baiduCell.Source = "https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png";
+            //var botCell = new Image();
+            //botCell.Source = "dotnet_bot.png";
+            //var baiduCell = new Image();
+            //baiduCell.Source = "https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png";
 
-            var youdaoCell = new Image();
+            var youdaoCell = new Image() { HeightRequest = 50 };
             youdaoCell.Source = "https://ydlunacommon-cdn.nosdn.127.net/cb776e6995f1c703706cf8c4c39a7520.png";
 
-            rootLayout.Children.Add(botCell);
-            rootLayout.Children.Add(baiduCell);
+            //rootLayout.Children.Add(botCell);
+            //rootLayout.Children.Add(baiduCell);
             rootLayout.Children.Add(youdaoCell);
-            rootLayout.Children.Add(new Label() { HeightRequest = 20, Text = index.ToString() });
+            var text = new Label() { HeightRequest = 50, Text = index.ToString(), BackgroundColor = Colors.Gray };
+            rootLayout.Children.Add(text);
+            var drag = new DragGestureRecognizer();
+            drag.CanDrag = true;
+            drag.DragStarting += Drag_DragStarting;
+            var drop = new DropGestureRecognizer();
+            drop.Drop += Drop_Drop;
+            drop.AllowDrop = true;
+            text.GestureRecognizers.Add(drag);
+            text.GestureRecognizers.Add(drop);
         }
+
+        ChangeY.Clicked += ChangeY_Clicked;
+        RemoveView.Clicked += RemoveView_Clicked;
+    }
+
+    private void RemoveView_Clicked(object sender, EventArgs e)
+    {
+        rootLayout.RemoveAt(3);
+    }
+
+    bool add = false;
+    private void ChangeY_Clicked(object sender, EventArgs e)
+    {
+        if(add)
+        {
+            emptyView.HeightRequest = 500;
+            add = false;
+        }
+        else
+        {
+            emptyView.HeightRequest = 300;
+            add = true;
+        }
+
+        ;
+    }
+
+    View drag;
+    private Grid emptyView;
+
+    private void Drag_DragStarting(object sender, DragStartingEventArgs e)
+    {
+        drag = (sender as DragGestureRecognizer).Parent as View;
+    }
+
+    private void Drop_Drop(object sender, DropEventArgs e)
+    {
+        //(drag.Parent as Layout).Remove(drag);
+        var target = (sender as DropGestureRecognizer).Parent as View;
+        var targetIndex = (target.Parent as Layout).IndexOf(target);
+        (target.Parent as Layout).Insert(targetIndex, drag);
     }
 }
